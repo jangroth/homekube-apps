347386e0ab6f3ff8f11bf7036394b8dbe48891d4
Jan Groth <jan.groth.de@gmail.com>
2025-08-24 20:45:16 +1000

    Metrics-server

--- manual/cilium-test/test.sh
+++ manual/cilium-test/test.sh
@@ -2,6 +2,8 @@
 
 set -ux
 
-curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.220:30745
-curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.221:30745
-curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.222:30745
+PORT=30002
+
+curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.220:$PORT
+curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.221:$PORT
+curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.222:$PORT
