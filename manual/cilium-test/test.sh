0e3354632c613b6d06f4d319f74bbeefd78a2ba9
Jan Groth <jan.groth.de@gmail.com>
2025-09-05 20:24:40 +1000

    Cilium test

--- manual/cilium-test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ux
-
-PORT=30002
-
-curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.220:$PORT
-curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.221:$PORT
-curl -s -o /dev/null -w "%{http_code}\n" --max-time 2 192.168.86.222:$PORT
